81f1bb4555718924a0e2925250ee227b65263c54
Chathura <chathura93@yahoo.com>
2023-08-17 15:46:59 +1200

    trying Google filament engine for android

---
+++ app/src/main/java/com/choota/googlefilamenttry/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.choota.googlefilamenttry
+
+import android.os.Bundle
+import android.view.SurfaceView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.choota.googlefilamenttry.ui.theme.GoogleFilamentTryTheme
+import com.google.android.filament.utils.Utils
+
+class MainActivity : ComponentActivity() {
+
+    companion object {
+        init {
+            Utils.init()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GoogleFilamentTryTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AndroidView(factory = { context ->
+                        val renderer = ModelRenderer()
+                        SurfaceView(context).apply {
+                            renderer.onSurfaceAvailable(this, lifecycle)
+                        }
+                    })
+                }
+            }
+        }
+    }
+}
